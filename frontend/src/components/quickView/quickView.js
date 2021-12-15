726419f8ffbd92b216fd56d8a4dbd6561419fdfa
sadiqali7140 <61231544+sadiqali7140@users.noreply.github.com>
2021-12-15 16:14:05 +0500

    don't care at this point

--- frontend/src/components/quickView/quickView.js
+++ frontend/src/components/quickView/quickView.js
@@ -1,23 +1,106 @@
 import React from "react";
 // import ReactDOM from "react-dom";
-import "./quickView.css"
+import "./quickView.css";
+import "../../index.css";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function QuickView() {
-    return(
-        <div>
-            <div className="Heading">
-                <div className="ActionItemContainer">
-                    
-                </div>,
-                <div className="ComingUpContainer">
-                    
-                </div>,
-                <div className="LastNPostContainer">
-
+  return (
+    <div>
+      <div className="Heading">
+        <div className="ActionItemContainer">
+          <h1 className="primary-font">Action Items</h1>
+          <h4 className="primary-font">Lorem Ipsum Dotor</h4>
+          <div>
+            <Splide
+              options={{
+                type: "loop",
+                pagination: false,
+                // height: "50vh",
+                // speed: '0',
+                autoplay: false,
+                // interval: '0',
+              }}
+            >
+              <SplideSlide>
+                <div>
+                  <div className="Stuff">
+                    <span>Task 1</span>
+                    <span>Very Important</span>
+                  </div>
+                  <div className="Stufff">
+                    <button>button</button>
+                  </div>
                 </div>
-            </div>
+              </SplideSlide>
+              <SplideSlide></SplideSlide>
+            </Splide>
+          </div>
         </div>
-    )
+        
+        <div className="ComingUpContainer">
+          <h1 className="primary-font">Coming Up</h1>
+          <h4 className="primary-font">Lorem Ipsum Dotor</h4>
+          <div>
+            <Splide
+              options={{
+                type: "loop",
+                pagination: false,
+                // height: "50vh",
+                // speed: '0',
+                autoplay: false,
+                // interval: '0',
+              }}
+            >
+              <SplideSlide>
+                <div>
+                  <div className="Stuff">
+                    <span>Task 1</span>
+                    <span>Very Important</span>
+                  </div>
+                  <div className="Stufff">
+                    <button>button</button>
+                  </div>
+                </div>
+              </SplideSlide>
+              <SplideSlide></SplideSlide>
+            </Splide>
+          </div>
+        </div>
+        
+        <div className="LastNPostContainer">
+          <h1 className="primary-font">Last 'N' Posts</h1>
+          <h4 className="primary-font">Lorem Ipsum Dotor</h4>
+          <div>
+            <Splide
+              options={{
+                type: "loop",
+                pagination: false,
+                // height: "50vh",
+                // speed: '0',
+                autoplay: false,
+                // interval: '0',
+              }}
+            >
+              <SplideSlide>
+                <div>
+                  <div className="Stuff">
+                    <span>Task 1</span>
+                    <span>Very Important</span>
+                  </div>
+                  <div className="Stufff">
+                    <button>button</button>
+                  </div>
+                </div>
+              </SplideSlide>
+              <SplideSlide></SplideSlide>
+            </Splide>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default QuickView
+export default QuickView;

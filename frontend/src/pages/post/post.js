0f456e7ec782107dcdb01aa053f1af2c6c1c07c5
sadiqali7140 <61231544+sadiqali7140@users.noreply.github.com>
2021-12-23 23:26:22 +0500

    dasda

--- frontend/src/pages/post/post.js
+++ frontend/src/pages/post/post.js
@@ -2,58 +2,69 @@ import React, { useEffect, useState } from "react";
 import "./post.css";
 import axios from "axios";
 
+let data = {
+  _id: "61c4b0064e0088fc92e4ab63",
+  image_url:
+    "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350",
+  title: "Title",
+  upload_date: "upload date",
+  caption: "Caption",
+  short_desciption: "Description is this",
+  approved: true,
+};
+
+let headers = { "x-access-token": sessionStorage.getItem("x-token") };
 
 export default function Post() {
-  const [data, setData] = useState({
-    image_url: "",
-    title: "",
-    description: "",
-    caption: "",
-    approved: "",
-    created_date: "", //remember to add thsi in form
-    upload_date: ""
-})
+  // let [content, setContent] = useState({
+  //   image_url: "",
+  //   title: "",
+  //   description: "",
+  //   caption: "",
+  //   approved: "",
+  //   created_date: "", //remember to add thsi in form
+  //   upload_date: "",
+  // });
 
-const [message, setMessage] = useState([])
-// const [headers, setHeaders] = useState({})
-let headers = {}
+  let [content, setContent] = useState([]);
 
-useEffect(() => {
-async function fetchMyAPI() {
-  await getContent() //first time call when page opens
-}
+  useEffect(() => {
+    async function fetchMyAPI() {
+      content = await getContent(data._id); //first time call when page opens
+    }
 
-headers = {
-    'Content-Type': 'application/json',
-    // 'x-access-token': sessionStorage.getItem('x-token')
-    'x-access-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgxNjFiYzJhYThhNTBiMjM4NGNiYmQiLCJlbWFpbCI6Indhc2lmLmthcmltQGhvdG1haWwuY29tIiwiaWF0IjoxNjQwMjc5NTQxLCJleHAiOjE2NDAzNjU5NDF9.IkdNB1o7RyAvVD-zcFff1ZWDMWzIb-FiEupelrBZjPA"
-}
+// headers = {
+    //   "x-access-token": sessionStorage.getItem("x-token"),
+    // };
 
-fetchMyAPI()
-}, [])
+    headers = {
+      "x-access-token":
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgxNjFiYzJhYThhNTBiMjM4NGNiYmQiLCJlbWFpbCI6Indhc2lmLmthcmltQGhvdG1haWwuY29tIiwiaWF0IjoxNjQwMjc5NTQxLCJleHAiOjE2NDAzNjU5NDF9.IkdNB1o7RyAvVD-zcFff1ZWDMWzIb-FiEupelrBZjPA",
+    };
 
-async function getContent() {
-  // let response = await axios.get('http://localhost:5000/content/' + {_id})
-  let response = await axios.get('http://localhost:5000/content/61c2a249cd784b96de249ca1', {headers: headers})
-  // console.log(response)
-  setData(response.data.data)
-}
-  
+    fetchMyAPI();
+  }, []);
+
+  async function getContent(id) {
+    let response = await axios.get("http://localhost:5000/content/" + id, { headers: headers });
+    setContent(response.data.data);
+  }
+
   return (
     <div>
       <div className="Base">
         <div>
           <div className="ImageContainer">
-            <img src={data.image_url} alt="new"></img>
+            <img src={content.image_url} alt="new"></img>
           </div>
           <div className="Text">
-            <h1 className="primary-font">{data.title}</h1>
-            <h4 className="primary-font">{data.upload_date}</h4>
-            <p className="primary-font">Caption: {data.caption}</p>
-            <p className="primary-font">Description: {data.description}</p>
+            <h1 className="primary-font">{content.title}</h1>
+            <h4 className="primary-font">{content.date}</h4>
+            <p className="primary-font">Caption: {content.caption}</p>
+            <p className="primary-font">Description: {content.description}</p>
           </div>
           <div className="Approval">
-            <h4 className="primary-font">Approval Status: {data.approved}</h4>
+            <h4 className="primary-font">Approval Status: {content.approved}</h4>
           </div>
           <div className="Comments"></div>
         </div>
